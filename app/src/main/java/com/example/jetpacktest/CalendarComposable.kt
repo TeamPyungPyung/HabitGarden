5438edb70d2e3a98b2c06a5f62977b6a753881c4
Minkyo Lee <redfowl@naver.com>
2022-05-18 20:18:51 +0900

    HistoryScreen implement
    - fixed habit property (days)
    - show only habit which is available on that day

--- app/src/main/java/com/example/jetpacktest/CalendarComposable.kt
+++ app/src/main/java/com/example/jetpacktest/CalendarComposable.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpacktest.ui.theme.JetpackTestTheme
 import io.github.boguszpawlowski.composecalendar.SelectableCalendar
 import io.github.boguszpawlowski.composecalendar.rememberSelectableCalendarState
+import kotlinx.datetime.LocalDate
 
 class CalendarComposable : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,11 +36,17 @@ fun HistoryScreen(habits: List<Habit>) {
         Text("${calendarState.selectionState.selection.joinToString { it.toString() }}")
         if (calendarState.selectionState.selection.size != 0) {
             Text("${calendarState.selectionState.selection[0].dayOfWeek.value}")
+            // Mon : 1, ... , Sun : 7
         }
         LazyColumn{
-            items(habits) { habit ->
-            HabitItem(habit = habit)
-        }}
+            if (calendarState.selectionState.selection.size != 0) {
+                items(habits) { habit ->
+                    if (habit.days[calendarState.selectionState.selection[0].dayOfWeek.value - 1]) {
+                        HabitItem(habit = habit)
+                    }
+                }
+            }
+        }
     }
 }
 
--- app/src/main/java/com/example/jetpacktest/SampleHabit.kt
+++ app/src/main/java/com/example/jetpacktest/SampleHabit.kt
@@ -6,27 +6,16 @@ data class Habit(
     val id: String = UUID.randomUUID().toString(),
     val habitName: String ="",
     var isAchieved: Boolean = false,
-    var days: Days,
-)
-
-class Days(
-    var Mon: Boolean = false,
-    var Tue: Boolean = false,
-    var Wed: Boolean = false,
-    var Thu: Boolean = false,
-    var Fri: Boolean = false,
-    var Sat: Boolean = false,
-    var Sun: Boolean = false,
-
+    var days: Array<Boolean> = arrayOf(false, false, false, false, false, false, false),
 )
 
 object SampleHabit {
     val habitSample = listOf(
-        Habit(habitName = "drink water", days = Days(Mon = true, Wed = true, Fri = true) ),
-        Habit(habitName = "go to gym", days = Days(Tue = true)),
-        Habit(habitName = "Study Android", days = Days(Thu = true)),
-        Habit(habitName = "go for a walk", days = Days(Sat = true)),
-        Habit(habitName = "cook for family", days = Days(Sun = true)),
+        Habit(habitName = "drink water", days = arrayOf(false, false, true, true, false, false, true)) ,
+        Habit(habitName = "go to gym", days = arrayOf(false, false, true, true, false, true, true)),
+        Habit(habitName = "Study Android", days = arrayOf(true, false, true, false, false, false, true)),
+        Habit(habitName = "go for a walk", days = arrayOf(false, true, true, true, false, false, true)),
+        Habit(habitName = "cook for family", days = arrayOf(false, false, true, true, true, false, true)),
 
     )
 }
